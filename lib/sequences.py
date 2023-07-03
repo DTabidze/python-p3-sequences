5e728978b81c1dc03528fea900a8ca72f1c9ea80
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-03 14:05:15 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci = [0,1]
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print ([0])
+    elif length == 2:
+        print ([0,1])
+    else:
+        index = 2
+        while len(fibonacci) < 10:
+            fibonacci.append(fibonacci[index - 1] + fibonacci[index - 2])
+            index+=1
+        print (fibonacci)
     pass
+
+print_fibonacci(10)
